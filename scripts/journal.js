1501124b4e618815f2c98d6471d7244ce8e630d0
Matthew Kroeger <mattk932009@gmail.com>
2020-01-21 21:31:05 -0600

    daily 3

--- scripts/journal.js
+++ scripts/journal.js
@@ -23,3 +23,26 @@ const newEntry2 = {
 entryLog.push(newEntry)
 entryLog.push(newEntry2)
 console.log(entryLog)
+
+const element = (comp, text, classes) => `<${comp} class="${classes}">${text}</${comp}>`
+
+const logEntry = jEntry => {
+    return `
+    <section class="jEntry">
+    ${element("div", `Date: ${jEntry.date}`, "date")}
+    ${element("div", `Concepts covered: ${jEntry.concepts}`, "concepts")}
+    ${element("div", `Entry: ${jEntry.entry}`, "entryText")}
+    ${element("div", `Mood: ${jEntry.mood}`, "mood")}
+    </section>`
+}
+
+const myEntries = document.querySelector(".entrylog")
+const renderJournalEntries = (entries) => {
+    entryLog.forEach(entry => {
+        myEntries.innerHTML += logEntry(entry)
+    });
+}
+
+// Invoke the render function
+console.log(myEntries)
+renderJournalEntries(entryLog)
