5f8d605dc3f5cdc6488d0936d1fd01da3fc8e8a9
Matthew Kroeger <mattk932009@gmail.com>
2020-02-05 16:43:12 -0600

    initial commit

--- scripts/data.js
+++ scripts/data.js
@@ -17,6 +17,26 @@ const API = {
         return fetch(`http://localhost:3000/entryLog/${id}`, {
             method: "DELETE"
         })
+    },
+    editEntry(updatedObject, id){
+        fetch(`http://localhost:3000/entryLog/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedObject)
+    })
+    .then(res => res.json())
+    .then(() => {
+        /*
+            Since this is the point in the code where you KNOW
+            the operation completed successfully, clear the
+            value of the hidden input field to that your
+            application is back to the state of creating instead
+            of editing
+        */
+        document.querySelector("#entryId").value = ""
+    })
     }
 }
 
